2646c3f577cc170b36c300d19ded9023105683ee
michael <michael.akinsete7@gmail.com>
2026-08-14 11:42:34 -0400

    (refactor) moved all functionalities regarding bcache_t into a header-only file. updated api

--- src/internal/bcache.h
+++ src/internal/bcache.h
@@ -9,41 +9,142 @@
 #ifndef bcache_h
 #define bcache_h
 
+
 #include    "common.h"
 #include    "error.h"
+
+#include    "../config/tsalloc_config.h"
 
 #include    "span.h"
 #include    "slab.h"
 #include    "pail.h"
 #include    "arenaconfig.h"
 
+
 /**
- * @brief   cache for small memory blocks.
+ * @brief   cache for small memory blocks
  */
 struct block_cache
 {
-    pail_t     *pails;      /**< array of pails for block management */
-    scache_t   *macro;      /**< pointer to the backend span cache */
-    size_t      nclasses;   /**< number of supported size classes */
+    pail_t         *pails;     
+    scache_t       *macro;    
+    ts_szclass_t    nszclasses; 
 };
 typedef struct block_cache  bcache_t;
 
 /**
+ * @brief   initializes a block cache
+ *
+ * @param   glob_state  pointer to the global allocation state
+ * @param   arena_cfg   pointer to the arena configuration
+ * @param   error_ctx   pointer to the error context
+ * @param   macro       pointer to backup span cache
+ * @param   auxil_mem   pointer to pre-allocated memory buffer for internal structures
+ * @param   cache       pointer to block cache instance to initialize
+ *
+ * @return  status code representing success or failure
+ */
+static inline tsalloc_err_t   
+bcache_init(
+    const glob_alloc_state_t   *glob_state,
+    const arena_cfg_t          *arena_cfg,
+    tsalloc_errctx_t           *error_ctx,
+    scache_t                   *macro,
+    byte_t                     *auxil_mem,
+    bcache_t                   *cache
+){
+    if (!auxil_mem)
+    {
+        set_tsalloc_error(
+            error_ctx,
+            "scache_init::scache.c nullptr axuil_mem argument",
+            TSALLOC_INVALID_ARGS
+        );
+        return TSALLOC_INVALID_ARGS;
+    }
+    
+    pail_t         *pails;
+    ts_szclass_t    nszclasses;
+
+    pails       = (pail_t*)auxil_mem;
+    nszclasses  = glob_state->nszclasses_slab;
+    *cache      = (bcache_t){
+            .pails      = pails,
+            .macro      = macro,
+            .nszclasses = nszclasses
+    };
+
+    tsalloc_err_t   ret;
+
+    for (ts_szclass_t i = 0; i < nszclasses; i++)
+    {
+        ret = pail_init(
+            glob_state, 
+            arena_cfg, 
+            error_ctx, 
+            macro, 
+            pails + i, 
+            i
+        );
+        if (ret != TSALLOC_SUCCESS)
+        {
+            for (ts_szclass_t j = 0; j < i; j++)
+            {
+                (void)pail_deinit(nullptr, pails + j);
+            }
+            append_tsalloc_error_trace(error_ctx);
+            return ret;
+        }
+    }
+
+    return TSALLOC_SUCCESS;
+}
+
+/**
+ * @brief   deinitializes a block cache
+ *
+ * @param   error_ctx   pointer to the error context
+ * @param   cache       pointer to block cache instance to deinitialize
+ *
+ * @return  status code representing success or failure
+ */
+static inline tsalloc_err_t
+bcache_deinit(
+    tsalloc_errctx_t   *error_ctx,
+    bcache_t           *cache
+){
+    tsalloc_err_t   ret1, ret2;
+
+    ret2    = TSALLOC_SUCCESS;
+    for (ts_szclass_t i = 0; i < cache->nszclasses; i++)
+    {
+        ret1    = pail_deinit(error_ctx, cache->pails + i);
+        if (ret1 != TSALLOC_SUCCESS)
+        {
+            ret2    = ret1;
+            append_tsalloc_error_trace(error_ctx);
+        }
+    }
+
+    return ret2;
+}
+
+/**
  * @brief   calculates required auxiliary memory size for the block cache
  *
- * @param   arena_cfg   arena configuration context
+ * @param   glob_state  pointer to the global allocation state
  *
  * @return  required memory size in bytes
  */
 static inline size_t
 bcache_auxil_mem_size(
-    arena_cfg_t        *arena_cfg
+    const glob_alloc_state_t   *glob_state
 ){
-    size_t  nbytes;
-    size_t  nclasses;
+    size_t          nbytes;
+    ts_szclass_t    nszclasses;
 
-    nclasses    = arena_cfg->tsalloc_cfg->nszclasses_slab;
-    nbytes      = sizeof(pail_t) * nclasses;
+    nszclasses  = glob_state->nszclasses_slab;
+    nbytes      = sizeof(pail_t) * nszclasses;
 
     return nbytes;
 }
@@ -51,41 +152,35 @@ bcache_auxil_mem_size(
 /**
  * @brief   retrieves a batch of memory blocks
  *
- * @param   error_ctx   error handling context
- * @param   arena_cfg   arena configuration context
- * @param   cache       block cache instance
- * @param   dest        pointer to store the batch array pointer
- * @param   szclass     size class index of the requested blocks
+ * @param   glob_state  pointer to the global allocation state
+ * @param   arena_cfg   pointer to the arena configuration
+ * @param   error_ctx   pointer to the error context
+ * @param   cache       pointer to block cache 
+ * @param   dest        array receiving the block addresses
+ * @param   szclass     size class of requested blocks
  * @param   nblocks     number of blocks to retrieve
  *
  * @return  status code representing success or failure
+ *
+ * @warning szclass must be a validated by caller
  */
 static inline tsalloc_err_t
 bcache_get_batch(
-    tsalloc_errctx_t   *error_ctx,
-    arena_cfg_t        *arena_cfg,
-    bcache_t           *cache,
-    byte_t            **dest,
-    tsalloc_szclass_t   szclass,
-    size_t              nblocks
-){
-    if (szclass > arena_cfg->tsalloc_cfg->nszclasses_slab)
-    {
-        set_tsalloc_error(
-            error_ctx,
-            "bcache_get_batch::bcache.h invalid slab szclass arguemnt",
-            TSALLOC_INVALID_ARGS
-        );
-        return TSALLOC_INVALID_ARGS;
-    }
-    
+    const glob_alloc_state_t   *glob_state,
+    const arena_cfg_t          *arena_cfg,
+    tsalloc_errctx_t           *error_ctx,
+    bcache_t                   *cache,
+    byte_t                    **dest,
+    ts_szclass_t                szclass,
+    size_t                      nblocks
+){    
     tsalloc_err_t   ret;
 
     ret = pail_get_batch(
-        error_ctx, 
+        glob_state, 
         arena_cfg, 
-        cache->macro, 
-        &(cache->pails[szclass]), 
+        error_ctx, 
+        cache->pails + szclass, 
         dest, 
         nblocks
     );
@@ -99,56 +194,32 @@ bcache_get_batch(
 }
 
 /**
- * @brief   initializes a block cache
+ * @brief   returns a memory block to the block cache
  *
- * @param   error_ctx   error handling context
- * @param   arena_cfg   arena configuration context
- * @param   macro       backend span cache for fulfilling slab allocations
- * @param   auxil_mem   pre-allocated memory buffer for internal structures
- * @param   cache       block cache instance to initialize
- *
- * @return  status code representing success or failure
+ * @param   glob_state  pointer to the global allocation state
+ * @param   arena_cfg   pointer to the arena configuration
+ * @param   error_ctx   pointer to the error context
+ * @param   cache       pointer to block cache
+ * @param   slab        slab containing the block
+ * @param   block       pointer to the memory block being returned
  */
-tsalloc_err_t   
-bcache_init(
-    tsalloc_errctx_t   *error_ctx,
-    arena_cfg_t        *arena_cfg,
-    scache_t           *macro,
-    byte_t             *auxil_mem,
-    bcache_t           *cache
-);
-
-/**
- * @brief   deinitializes a block cache.
- *
- * @param   error_ctx   error handling context
- * @param   cache       block cache instance to deinitialize
- *
- * @return  status code representing success or failure
- */
-tsalloc_err_t
-bcache_deinit(
-    tsalloc_errctx_t   *error_ctx,
-    bcache_t           *cache
-);
-
-/**
- * @brief   frees a memory block back to the block cache
- *
- * @param   error_ctx   error handling context
- * @param   arena_cfg   arena configuration context
- * @param   cache       block cache instance
- * @param   block       pointer to the memory block being freed
- *
- * @return  status code representing success or failure
- */
-tsalloc_err_t
+static inline void 
 bcache_put_block(
-    tsalloc_errctx_t   *error_ctx,
-    arena_cfg_t        *arena_cfg,
-    bcache_t           *cache,
-    byte_t             *block
-);
+    const glob_alloc_state_t   *glob_state,
+    const arena_cfg_t          *arena_cfg,
+    tsalloc_errctx_t           *error_ctx,
+    bcache_t                   *cache,
+    span_t                     *slab,
+    byte_t                     *block
+){
+    pail_put_block(
+        glob_state, 
+        arena_cfg, 
+        cache->pails + ((ts_szclass_t)slab->slabmeta->szclass), 
+        slab, 
+        block
+    );
+}
 
 
-#endif  //bcache_h
+#endif  //BCACHE_H
